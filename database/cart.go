e667cca4d87c1d0b08d56f6adba1a2b255560e65
fredele20 <victorisholaoladele@gmail.com>
2023-02-06 02:59:24 +0100

    more implementations

--- database/cart.go
+++ database/cart.go
@@ -1,6 +1,15 @@
 package database
 
-import "errors"
+import (
+	"context"
+	"errors"
+	"log"
+
+	"github.com/fredele20/e-commerce-cart/models"
+	"go.mongodb.org/mongo-driver/bson"
+	"go.mongodb.org/mongo-driver/bson/primitive"
+	"go.mongodb.org/mongo-driver/mongo"
+)
 
 var (
 	ErrProductNotFound       = errors.New("product not found")
@@ -12,7 +21,36 @@ var (
 	ErrCantUpdateUser        = errors.New("cannot add this product to the cart")
 )
 
-func AddProductToCart() {}
+func AddProductToCart(ctx context.Context, productCollection, userCollection *mongo.Collection, productId primitive.ObjectID, userId string) error {
+	searchFromDB, err := productCollection.Find(ctx, bson.M{"_id":productId})
+	if err != nil {
+		log.Println(err)
+		return ErrProductNotFound
+	}
+
+	var productCart []models.ProductUser
+	err = searchFromDB.All(ctx, &productCart)
+	if err != nil {
+		log.Println(err)
+		return ErrProductDecodingFailed
+	}
+
+	id, err := primitive.ObjectIDFromHex(userId)
+	if err != nil {
+		log.Println(err)
+		return ErrUserIdNotValid
+	}
+
+	filter := bson.D{primitive.E{Key: "_id", Value: id}}
+	update := bson.D{{Key: "$push", Value: bson.D{primitive.E{Key: "usercart", Value: bson.D{{Key: "$each", Value: productCart}}}}}}
+
+	_, err = userCollection.UpdateOne(ctx, filter, update)
+	if err != nil {
+		return ErrCantUpdateUser
+	}
+
+	return nil
+}
 
 func RemoveCartItem() {}
 
--- middleware/middleware.go
+++ middleware/middleware.go
@@ -1,1 +1,30 @@
 package middleware
+
+import (
+	"net/http"
+
+	"github.com/fredele20/e-commerce-cart/tokens"
+	"github.com/gin-gonic/gin"
+)
+
+func Authentication() gin.HandlerFunc {
+	return func(ctx *gin.Context) {
+		clientToken := ctx.Request.Header.Get("token")
+		if clientToken == "" {
+			ctx.JSON(http.StatusInternalServerError, gin.H{"error": "no authorization header provided"})
+			ctx.Abort()
+			return
+		}
+
+		claims, err := tokens.ValidateToken(clientToken)
+		if err != "" {
+			ctx.JSON(http.StatusInternalServerError, gin.H{"error": err})
+			ctx.Abort()
+			return
+		}
+
+		ctx.Set("email", claims.Email)
+		ctx.Set("uid", claims.Uid)
+		ctx.Next()
+	}
+}
